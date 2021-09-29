ec1b89009ca8b4d1090bdee831fce4ab8c79752e
Rithik <isrithikta@gmail.com>
2021-09-28 22:30:29 -0400

    Fixed Indentation and added gitcommit in requirements

--- code/gits_push.py
+++ code/gits_push.py
@@ -4,6 +4,7 @@ import helper
 import git
 from pathlib import Path
 
+
 def gits_push(args):
     """
     This function is used to push local changes to remote branch.
@@ -37,35 +38,34 @@ def gits_push(args):
         stdout, stderr = process2.communicate()
         print(stdout.decode("utf-8"))
 
-        #Creating tests
-        #Ensure test folder exists
+        # Creating tests
+        # Ensure test folder exists
         Path("test").mkdir(parents=True, exist_ok=True)
 
-        #Get the commited files
+        # Get the commited files
         repo = git.Repo("")
         lastcommit = repo.head.commit
         commited_files = None
         for commit in list(repo.iter_commits()):
-            if commit==lastcommit:
-                commited_files =commit.stats.files
-        #If commited files are found, check them one by one and ensure tests exist under /test
+            if commit == lastcommit:
+                commited_files = commit.stats.files
+        # If commited files are found, check them one by one and ensure tests exist under /test
         if commited_files:
             for commited_file in commited_files:
                 file_path = str(commited_file)
-                print("Checking if tests exist for: "+file_path)
-                #Ignore files already in the test folder
+                print("Checking if tests exist for: " + file_path)
+                # Ignore files already in the test folder
                 if(file_path.startswith("test")):
                     continue
-                #Check if its a python type file
-                if(file_path.endswith(".py") and not Path("test/"+file_path).is_file()):
-                    #Ensure proper test file exists
+                # Check if its a python type file
+                if(file_path.endswith(".py") and not Path("test/" + file_path).is_file()):
+                    # Ensure proper test file exists
                     print("Creating test files")
-                    Path("test/"+file_path).parent.mkdir(parents=True, exist_ok=True)
-                    f = open("test/"+file_path, "w")
-                    f.write("#Test for "+file_path)
+                    Path("test/" + file_path).parent.mkdir(parents=True, exist_ok=True)
+                    f = open("test/" + file_path, "w")
+                    f.write("#Test for " + file_path)
                     f.close()
-                    
-                
+
     except Exception as e:
         print("ERROR: gits push command caught an exception")
         print("ERROR: {}".format(str(e)))
