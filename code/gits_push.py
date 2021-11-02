538fed58b43d585db88bfacbf3508c26a9959f32
Neha Agarwal <89555333+nehaAgarwal96@users.noreply.github.com>
2021-11-02 17:27:33 -0400

    Update gits_push.py
    Implemented functions to calculate grade using the score and displayed the score metrics in a tabulated manner.

--- code/gits_push.py
+++ code/gits_push.py
@@ -1,7 +1,29 @@
 import subprocess
 from subprocess import PIPE
 import helper
+from tabulate import tabulate
 
+def calculate_grade(score):
+    if score >= 95:
+        grade = 'A+'
+    elif score in range(90, 95):
+        grade = 'A'
+    elif score in range(85, 90):
+        grade = 'B+'
+    elif score in range(80, 85):
+        grade = 'B'
+    elif score in range(75, 80):
+        grade = 'C+'
+    elif score in range(70, 75):
+        grade = 'C'
+    elif score in range(65, 70):
+        grade = 'D+'
+    elif score in range(60, 65):
+        grade = 'D'
+    else:
+        grade = 'F'
+
+    return grade
 
 def gits_push(args):
     """
@@ -51,6 +73,21 @@ def gits_push(args):
 
         stdout, stderr = process2.communicate()
         print(stdout.decode("utf-8"))
+                     
+        grade = calculate_grade(total_score)
+
+        print("Repository grade is :", grade)
+
+        table = [['README.md', '1', readme_present],
+                    ['CONTRIBUTING.md', '1', contribution_present], ['CODE_OF_CONDUCT.md', '1', coc_present],
+                    ['LICENSE.md', '1', license_present], ['CITATION.md', '1', citation_present],
+                    ['.gitignore', '1', gitignore_present], ['Issues Closed (Last 30 days)', '1', issue_score],
+                    ['TOTAL SCORE', (issue_score + file_count), total_score]
+                    ]
+        print("\n")
+        print(tabulate(table, headers = ['Item', 'Weight', 'Score']))
+        print("\n")
+
 
     except Exception as e:
         print("ERROR: gits push command caught an exception")
