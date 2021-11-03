1b14ae335b7139cc954e1683e22576b94126fe06
hiralbhanu <hiralbhanushali13@gmail.com>
2021-11-02 21:18:23 -0400

    Create gits_list.py

---
+++ code/gits_list.py
@@ -0,0 +1,39 @@
+import gits_logging
+
+
+def gits_list_commands(args):
+    """
+    Function that prints list of custom commands
+    to user console
+    """
+    print("GITS Custom Commands")
+    print("gits.py")
+    print("gits_add.py")
+    print("gits_branch.py")
+    print("gits_commit.py")
+    print("gits_create.py")
+    print("gits_creategoodrepo.py")
+    print("gits_custom.py")
+    print("gits_delete.py")
+    print("gits_diff.py")
+    print("gits_hello.py")
+    print("gits_init.py")
+    print("gits_list.py")
+    print("gits_logging.py")
+    print("gits_merge.py")
+    print("gits_profile.py")
+    print("gits_pull.py")
+    print("gits_push.py")
+    print("gits_rebase.py")
+    print("gits_reset.py")
+    print("gits_set.py")
+    print("gits_setupstream.py")
+    print("gits_status.py")
+    print("gits_super_reset.py")
+    print("gits_switch.py")
+    print("gits_sync.py")
+    print("gits_track.py")
+    print("gits_undo.py")
+    print("gits_untrack.py")
+
+    gits_logging.gits_logger.info("List of Custom Command ")
