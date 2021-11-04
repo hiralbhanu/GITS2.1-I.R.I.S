9ddd3efc5108ffb39d4a94cf25a2bc382790a23d
Hrushabhsingh Chouhan <43321682+hrushabhchouhan@users.noreply.github.com>
2021-11-04 18:08:38 -0400

    Add files via upload
    Added a functionality to list all the available gits commands

--- code/gits_list.py
+++ code/gits_list.py
@@ -1,4 +1,5 @@
 import gits_logging
+import os
 
 
 def gits_list_commands(args):
@@ -7,33 +8,11 @@ def gits_list_commands(args):
     to user console
     """
     print("GITS Custom Commands")
-    print("gits.py")
-    print("gits_add.py")
-    print("gits_branch.py")
-    print("gits_commit.py")
-    print("gits_create.py")
-    print("gits_creategoodrepo.py")
-    print("gits_custom.py")
-    print("gits_delete.py")
-    print("gits_diff.py")
-    print("gits_hello.py")
-    print("gits_init.py")
-    print("gits_list.py")
-    print("gits_logging.py")
-    print("gits_merge.py")
-    print("gits_profile.py")
-    print("gits_pull.py")
-    print("gits_push.py")
-    print("gits_rebase.py")
-    print("gits_reset.py")
-    print("gits_set.py")
-    print("gits_setupstream.py")
-    print("gits_status.py")
-    print("gits_super_reset.py")
-    print("gits_switch.py")
-    print("gits_sync.py")
-    print("gits_track.py")
-    print("gits_undo.py")
-    print("gits_untrack.py")
-
+    path = "/Users/hiral/Applications/GITS2.1-I.R.I.S/code"
+    files = os.listdir(path)
+    for f in files:
+        if "_" in f and "gits" in f:
+            f = f.replace('_', ' ')
+            f = f[0:-3]
+            print(f)
     gits_logging.gits_logger.info("List of Custom Command ")
